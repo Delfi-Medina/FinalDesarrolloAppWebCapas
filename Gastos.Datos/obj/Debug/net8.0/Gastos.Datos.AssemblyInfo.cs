3c20e7ee6512a90e4fe003ff0e39de49a34d5407
Delfi-medina <delfimedina02@gmail.com>
2024-07-08 18:14:24 -0300

    Capa de Negocios Terminada

--- Gastos.Datos/obj/Debug/net8.0/Gastos.Datos.AssemblyInfo.cs
+++ Gastos.Datos/obj/Debug/net8.0/Gastos.Datos.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gastos.Datos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2973104f39e74b0964738ac4758d525bedf3aa9")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b46a28ee8608b91ce6d06d8a34700634aae0d49a")]
+>>>>>>> develop
 [assembly: System.Reflection.AssemblyProductAttribute("Gastos.Datos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gastos.Datos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Gastos.Negocios/Class1.cs
+++ Gastos.Negocios/Class1.cs
@@ -1,7 +1,40 @@
-﻿namespace Gastos.Negocios
-{
-    public class Class1
-    {
+﻿using Gastos.Datos;
+using Gastos.Interfaces;
 
+namespace Gastos.Negocios
+{
+    public class GastoServicio
+    {
+        private readonly IGastoRepository _gastoRepository;
+
+        public GastoServicio (IGastoRepository gastoRepository)
+        {
+            _gastoRepository = gastoRepository;
+        }
+        public async Task<IEnumerable<Gasto>> ObtenerTodosLosGastosAsync()
+        {
+            return await _gastoRepository.GetAllGastosAsync();
+        }
+
+        public async Task<Gasto> ObtenerGastoPorIdAsync(int id)
+        {
+            return await _gastoRepository.GetGastoByIdAsync(id);
+        }
+
+        public async Task AgregarGastoAsync(Gasto gasto)
+        {
+            await _gastoRepository.AddGastoAsync(gasto);
+        }
+
+        public async Task ActualizarGastoAsync(Gasto gasto)
+        {
+            await _gastoRepository.UpdateGastoAsync(gasto);
+        }
+
+        public async Task EliminarGastoAsync(int id)
+        {
+            await _gastoRepository.DeleteGastoAsync(id);
+        }
     }
 }
+
