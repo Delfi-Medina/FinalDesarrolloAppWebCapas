105296e74d26a6052283ae003e129365af94f0e7
Delfi-medina <delfimedina02@gmail.com>
2024-07-08 17:45:30 -0300

    Manejo de Erorrer

--- Gastos.Negocios/obj/Debug/net8.0/Gastos.Negocios.AssemblyInfo.cs
+++ Gastos.Negocios/obj/Debug/net8.0/Gastos.Negocios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gastos.Negocios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b46a28ee8608b91ce6d06d8a34700634aae0d49a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2973104f39e74b0964738ac4758d525bedf3aa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gastos.Negocios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gastos.Negocios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
